a9471ae0b363b85f6466a5780a74b6457cae723f
Steven Blom <steven.blom@drawboard.com>
2021-11-19 13:25:37 +1030

    Add project files.

---
+++ WebView2InWinUI3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WebView2InWinUI3
+{
+	/// <summary>
+	/// An empty window that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainWindow : Window
+	{
+		public event Action XamlReady;
+
+		public MainWindow()
+		{
+			this.InitializeComponent();
+		}
+
+		public async Task NavigateToFirstPage()
+		{
+			WebView.CoreWebView2Initialized += (_, args) =>
+			{
+				if (args.Exception != null)
+					Debug.Assert(args.Exception == null, args.Exception?.Message);
+			};
+			await WebView.EnsureCoreWebView2Async();
+			WebView.CoreWebView2.Navigate("http://www.google.com");
+		}
+
+		private void OnXamlReady(object sender, RoutedEventArgs e)
+		{
+			XamlReady?.Invoke();
+		}
+	}
+	
+}
---
+++ WebView2InWinUI3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+namespace WebView2InWinUI3;
+
+#if DISABLE_XAML_GENERATED_MAIN
+public static class Program
+{
+	[DllImport("Microsoft.ui.xaml.dll")]
+	private static extern void XamlCheckProcessRequirements();
+	
+	[STAThread]
+	//static void Main(string[] args) //Works
+	static async Task Main(string[] args) //Doesn't Work
+	{
+		Debug.WriteLine(Thread.CurrentThread.GetApartmentState());
+		XamlCheckProcessRequirements();
+
+		WinRT.ComWrappersSupport.InitializeComWrappers();
+			
+		Application.Start((p) =>
+		{
+			var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+			SynchronizationContext.SetSynchronizationContext(context);
+			new App();
+		});
+	}
+}
+#endif
